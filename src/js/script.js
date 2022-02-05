5683367b66ef22cab105a5087a8559f216989608
Irina <i_r_i_n_a_nikitina@mail.ru>
2022-02-05 19:28:21 +0300

    carousel section with tiny-slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,18 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1000,
-        slidesToShow: 1,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icon/carousel_section/left_arrow.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icon/carousel_section/right_arrow.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                  dots: true,
-                  arrows: false
-                }
-            }
-        ]
-    });
+"use strict";
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    navPosition: 'bottom'
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', () => {
+    slider.goTo('next');
 });

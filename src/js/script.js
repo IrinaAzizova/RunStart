e13c075429e956732eb5c53433a8e6a899626122
Irina <i_r_i_n_a_nikitina@mail.ru>
2022-02-07 23:23:10 +0300

    catalogue section

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,8 @@
 "use strict";
 
+
+/********** slider ********/
+
 const slider = tns({
     container: '.carousel__inner',
     items: 1,
@@ -15,4 +18,30 @@ document.querySelector('.prev').addEventListener('click', () => {
 
 document.querySelector('.next').addEventListener('click', () => {
     slider.goTo('next');
+});
+
+
+/********** tabs **********/
+
+$(function() {
+  
+    $('ul.catalogue__tabs').on('click', 'li:not(.catalogue__tab_active)', function() {
+      $(this)
+        .addClass('catalogue__tab_active').siblings().removeClass('catalogue__tab_active')
+        .closest('div.container').find('div.catalogue__content').removeClass('active').eq($(this).index()).addClass('active');
+        $('.catalogue-item__content').addClass('active');
+        $('.catalogue-item__back-side').removeClass('active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalogue-item__back-side').eq(i).toggleClass('active');
+                $('.catalogue-item__content').eq(i).toggleClass('active');
+            });
+        });
+    };
+    toggleSlide('.catalogue-item__link');
+    toggleSlide('.catalogue-item__back-link');
 });
